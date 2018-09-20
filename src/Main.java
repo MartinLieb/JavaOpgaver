d2c100d4d80879e316bb082b0c0b7c8bd16a86c1
Martin Pedersen <42993919+MartinLieb@users.noreply.github.com>
2018-09-20 10:58:15 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Java,");
+        System.out.println("welcome to computer science ");
+        System.out.println("and programming is fun");
+    }
+}
---
+++ src/opgave12.java
@@ -0,0 +1,11 @@
+public class opgave12 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to java");
+        System.out.println("Welcome to java");
+        System.out.println("Welcome to java");
+        System.out.println("Welcome to java");
+        System.out.println("Welcome to java");
+    }
+}
